c3577efa3c94ae2b9b950c263767b913840bbe1c
Ciaran Sweeney <ciaran2293@yahoo.com>
2021-05-16 23:38:51 +0100

    Basic Springboot app working

---
+++ src/main/java/com/ciaran/connectfive/ConnectFiveApplication.java
@@ -0,0 +1,13 @@
+package com.ciaran.connectfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciaran/connectfive/ConnectFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaran.connectfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
